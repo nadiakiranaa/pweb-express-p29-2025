1fd12c42ca09d306a0c11cc32880d776b3f63fcf
nadiakiranaa <nadiakirana280@gmail.com>
2025-10-22 03:29:53 +0700

    push dulu wes

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import genreRoutes from './routes/genreRoutes';
+// import transactionRoutes from './routes/transactionRoutes';
 
 const app = express();
 
@@ -6,10 +8,15 @@ app.use(express.json());
 
 // check endpoint
 app.get("/", (_, response) => {
-  response.status(200).send("Server is up and running 💫");
+	response.status(200).send("Server is up and running 💫");
 });
+
+// mount genre routes
+app.use('/genre', genreRoutes);
+
+// app.use('/transactions', transactionRoutes);
 
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Express is running on Port ${PORT}`);
+	console.log(`Express is running on Port ${PORT}`);
 });
---
+++ routes/transactionRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+// import { createTransaction, getAllTransactions, getTransactionDetail, getTransactionStatistic } from '../controller/transactionController';
+
+// const router = Router();
+
+// // POST /genre -> create a new genre
+// router.post('/', createTransaction);
+// router.get('/', getAllTransactions);
+// router.get('/statistic', getTransactionStatistic);
+// router.get('/:id', getTransactionDetail);
+
+// export default router;
